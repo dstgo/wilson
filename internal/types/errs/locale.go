66d190015705ef29ed70154298e40c9bf0c146e6
246859 <2633565580@qq.com>
2023-10-24 15:51:25 +0800

    fix(errs): fix recursive error call

---
+++ internal/types/errs/locale.go
@@ -0,0 +1,52 @@
+package errs
+
+func NewError() LocaleError {
+	return LocaleError{}
+}
+
+func NewI18nError(i18n string) LocaleError {
+	return NewError().I18n(i18n)
+}
+
+// LocaleError
+// a response error wrap
+// Err field
+type LocaleError struct {
+	ErrorCode  int
+	HttpStatus int
+	LangCode   string
+	Fb         string
+	Err        error
+}
+
+func (e LocaleError) Code(code int) LocaleError {
+	e.ErrorCode = code
+	return e
+}
+
+func (e LocaleError) Status(status int) LocaleError {
+	e.HttpStatus = status
+	return e
+}
+
+func (e LocaleError) I18n(langCode string) LocaleError {
+	e.LangCode = langCode
+	return e
+}
+
+func (e LocaleError) FallBack(fallback string) LocaleError {
+	e.Fb = fallback
+	return e
+}
+
+func (e LocaleError) Wrap(err error) LocaleError {
+	e.Err = err
+	return e
+}
+
+func (e LocaleError) Error() string {
+	if e.Err != nil {
+		return e.Err.Error()
+	}
+	return e.Fb
+}
