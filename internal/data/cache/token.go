9ae5ce00dada26bce8d1e7250c6286d64fd5f140
246859 <2633565580@qq.com>
2023-10-27 22:02:54 +0800

    feat(cache): token cache supported TTL,Key method

--- internal/data/cache/token.go
+++ internal/data/cache/token.go
@@ -6,31 +6,42 @@ import (
 	"fmt"
 	"github.com/dstgo/wilson/internal/data"
 	"github.com/go-redis/redis/v8"
-	"github.com/google/wire"
 	"time"
 )
 
 type TokenCache interface {
+	TTL(ctx context.Context, tokenId string) (time.Duration, error)
+	Key(tokenId string) string
 	Get(ctx context.Context, tokenId string) (string, bool, error)
 	Set(ctx context.Context, tokenId string, token string, expire time.Duration) error
 	Del(ctx context.Context, tokenId string) error
 }
 
-func TokenCacheKey(tokenId string) string {
-	return fmt.Sprintf("token:%s", tokenId)
+func NewAccessTokenCache(d *data.DataSource) RedisTokenCache {
+	return RedisTokenCache{cache: d.Redis(), keyFn: AccessTokenCacheKey}
 }
 
-var TokenCacheProvider = wire.NewSet(
-	NewRedisTokenCache,
-	wire.Bind(new(TokenCache), new(RedisTokenCache)),
-)
-
-func NewRedisTokenCache(d *data.DataSource) RedisTokenCache {
-	return RedisTokenCache{cache: d.Redis()}
+func NewRefreshTokenCache(d *data.DataSource) RedisTokenCache {
+	return RedisTokenCache{cache: d.Redis(), keyFn: RefreshTokenCacheKey}
 }
+
+var tokenCache TokenCache = RedisTokenCache{}
 
 type RedisTokenCache struct {
 	cache *redis.Client
+	keyFn func(key string) string
+}
+
+func (t RedisTokenCache) TTL(ctx context.Context, tokenId string) (time.Duration, error) {
+	ttl := t.cache.TTL(ctx, t.keyFn(tokenId))
+	return ttl.Val(), ttl.Err()
+}
+
+func (t RedisTokenCache) Key(tokenId string) string {
+	if t.keyFn != nil {
+		return t.keyFn(tokenId)
+	}
+	return ""
 }
 
 func (t RedisTokenCache) Get(ctx context.Context, tokenId string) (string, bool, error) {
@@ -39,7 +50,7 @@ func (t RedisTokenCache) Get(ctx context.Context, tokenId string) (string, bool,
 		err   error
 		exist bool
 	)
-	op := t.cache.Get(ctx, TokenCacheKey(tokenId))
+	op := t.cache.Get(ctx, t.keyFn(tokenId))
 
 	if errors.Is(op.Err(), redis.Nil) {
 		exist = false
@@ -54,9 +65,17 @@ func (t RedisTokenCache) Get(ctx context.Context, tokenId string) (string, bool,
 }
 
 func (t RedisTokenCache) Set(ctx context.Context, tokenId string, token string, expire time.Duration) error {
-	return t.cache.Set(ctx, TokenCacheKey(tokenId), token, expire).Err()
+	return t.cache.Set(ctx, t.keyFn(tokenId), token, expire).Err()
 }
 
 func (t RedisTokenCache) Del(ctx context.Context, tokenId string) error {
-	return t.cache.Del(ctx, TokenCacheKey(tokenId)).Err()
+	return t.cache.Del(ctx, t.keyFn(tokenId)).Err()
+}
+
+func AccessTokenCacheKey(tokenId string) string {
+	return fmt.Sprintf("token:%s", tokenId)
+}
+
+func RefreshTokenCacheKey(tokenId string) string {
+	return fmt.Sprintf("refresh:%s", tokenId)
 }
