a8cc77befc90de3b342d2533c52473fd1a4f5bc0
246859 <2633565580@qq.com>
2024-01-23 12:37:36 +0800

    fix(helper): fix req context key

--- internal/core/authen/helper.go
+++ internal/core/authen/helper.go
@@ -11,10 +11,7 @@ const (
 	contextKeyInfo = "gin.context.key.info"
 )
 
-func SetContextAPIInfo(ctx *gin.Context, key entity.APIKey) {
-	ctx.Set(contextTokenInfo, key)
-}
-
+// GetContextTokenInfo returns the token info in current request
 func GetContextTokenInfo(ctx *gin.Context) UserClaims {
 	value, _ := ctx.Get(contextTokenInfo)
 	claims, _ := value.(UserClaims)
@@ -25,8 +22,13 @@ func SetContextTokenInfo(ctx *gin.Context, claims UserClaims) {
 	ctx.Set(contextTokenInfo, claims)
 }
 
+// GetContextKeyInfo returns the key info in current request
 func GetContextKeyInfo(ctx *gin.Context) entity.APIKey {
-	value, _ := ctx.Get(contextTokenInfo)
+	value, _ := ctx.Get(contextKeyInfo)
 	key, _ := value.(entity.APIKey)
 	return key
+}
+
+func SetContextAPIInfo(ctx *gin.Context, key entity.APIKey) {
+	ctx.Set(contextKeyInfo, key)
 }
